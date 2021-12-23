5b607fa55fe9ee9188b572838ed8c4f3f7de5188
TarunYellapu <tarunyellapu007@gmail.com>
2021-12-23 23:39:20 +0530

    icon changes

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -31,7 +31,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "💀" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "ﮊ" };
 
 static const Rule rules[] = {
 	/* xprop(1):
